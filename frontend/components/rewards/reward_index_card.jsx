d6b80ba533e0410bc7e309f554c1415f03d6dfd8
Aaron Farber <aaronfarber@Aarons-MacBook-Pro.local>
2017-03-23 09:30:21 -0700

    rewards rendering, some css done

--- frontend/components/rewards/reward_index_card.jsx
+++ frontend/components/rewards/reward_index_card.jsx
@@ -10,10 +10,17 @@ class RewardIndexCard extends React.Component {
     const {reward} = this.props;
     return (
       <div className="reward-index-card">
-        <li><p className="reward-card-price">${reward.price}<span className='reward-currency'> USD</span></p></li>
+        <li><p className="reward-card-price">${reward.price}<span className='reward-currency'> USD</span>
+          <span className='shipping-text'> + Shipping</span>
+          </p>
+        </li>
         <li><p className="reward-card-title">{reward.title}</p></li>
         <li><p className="reward-card-description">{reward.description}</p></li>
-        <li><p className="reward-card-amount">{reward.amount}</p></li>
+        <br/>
+        <li>
+          <p className='reward-supply'>{reward.supply} available</p>
+          <p className='reward-supply'>Ships worldwide</p>
+        </li>
       </div>
     );
   }
