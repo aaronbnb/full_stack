39dfbc071e52dc0d3f135f675c019a9ffebbcca8
Aaron Farber <aaronfarber9@gmail.com>
2017-06-28 17:37:16 -0700

    contributions backend working

--- app/controllers/api/contributions_controller.rb
+++ app/controllers/api/contributions_controller.rb
@@ -3,11 +3,9 @@ class Api::ContributionsController < ApplicationController
   def create
     @contribution = Contribution.new(contribute_params)
     if @contribution.save
-      debugger
       campaign = Campaign.find_by_id(@contribution.campaign_id)
-      campaign.status += contribution.amount
-      debugger
-      render :show
+      campaign.status += @contribution.amount
+      campaign.save
     else
       render json: @contribution.errors.full_messages, status: 422
     end
