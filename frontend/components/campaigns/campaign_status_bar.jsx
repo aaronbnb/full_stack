3e54595a51591162fbcd4a3ce74a56b9f8e999f8
Aaron Farber <aaronfarber9@gmail.com>
2017-07-22 20:15:53 -0700

    heroku build errors

--- frontend/components/campaigns/campaign_status_bar.jsx
+++ frontend/components/campaigns/campaign_status_bar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Progress from 'react-progressbar';
+import {Progress} from 'react-progressbar';
 
 class CampaignStatusBar extends React.Component {
   constructor(props) {
